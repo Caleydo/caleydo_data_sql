b01b8a6e069e995ecd58da4773e14286d0ab6e86
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-10-06 11:18:28 +0200

    disable gevent sqlalchemy patch, because it causes a weird connection error with psycopg2==2.6.1

--- sql.py
+++ sql.py
@@ -4,8 +4,8 @@ import caleydo_server.range as ranges
 import itertools
 import sqlalchemy
 #patch sqlalchemy for better parallelism using gevent
-import sqlalchemy_gevent
-sqlalchemy_gevent.patch_all()
+#import sqlalchemy_gevent
+#sqlalchemy_gevent.patch_all()
 
 from caleydo_server.dataset_def import ADataSetEntry, ADataSetProvider
 
